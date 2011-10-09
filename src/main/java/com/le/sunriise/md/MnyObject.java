7547b8d49b28b1f155cc03b3466b420ade9ba954
hleofxquotes <hleofxquotes@gmail.com>
2011-10-09 16:37:07 +0000

    Succesfully generated QIF for all non-investments accounts.

--- src/main/java/com/le/sunriise/md/MnyObject.java
+++
@@ -1,8 +0,0 @@
-package com.le.sunriise.md;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public abstract class MnyObject {
-    protected Map<String, Object> columnValues = new HashMap<String, Object>();
-}
--- src/main/java/com/le/sunriise/md/TransactionFilter.java
+++
@@ -1,9 +0,0 @@
-package com.le.sunriise.md;
-
-import java.util.Map;
-
-public interface TransactionFilter {
-
-    boolean accept(Transaction transaction, Map<String, Object> row);
-
-}
